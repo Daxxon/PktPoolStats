f286ed0b185bc89925372cc962f18abea44f3fab
Daniel Waggoner <daxxon37@hotmail.com>
2021-10-28 21:38:31 -0400

    JSON to hashmap

--- src/main/java/com/github/daxxon/PKTStats/Controllers/WebController.java
+++ src/main/java/com/github/daxxon/PKTStats/Controllers/WebController.java
@@ -2,15 +2,27 @@ package com.github.daxxon.PKTStats.Controllers;
 
 import com.github.daxxon.PKTStats.HTTPClient;
 import com.github.daxxon.PKTStats.PoolStatResponse;
+import org.json.JSONObject;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class WebController {
 
+  /*
+  Returns the hashes per second per pool
+  should return a json that looks like:
+  {
+    "pktco": 123378912638921739
+    "other": 23409328403982
+    "other2": 324039284302984
+  }
+  */
   @GetMapping("/")
-  public PoolStatResponse home(Model model) throws Exception {
+  public HashMap<String, Integer> home(Model model) throws Exception {
     HTTPClient httpClient = new HTTPClient();
     return httpClient.getPoolStats();
   }
--- src/main/java/com/github/daxxon/PKTStats/HTTPClient.java
+++ src/main/java/com/github/daxxon/PKTStats/HTTPClient.java
@@ -10,6 +10,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HTTPClient {
@@ -19,7 +20,7 @@ public class HTTPClient {
     .version(HttpClient.Version.HTTP_2)
     .build();
 
-  public PoolStatResponse sendGet(String myUri) throws Exception {
+  public PoolStatResponse getPoolStats(String myUri) throws Exception {
 
     HttpRequest request = HttpRequest.newBuilder()
       .GET()
@@ -41,43 +42,11 @@ public class HTTPClient {
 
   }
 
-  public JSONObject getPoolStats(){
-    this.sendGet("http://paymaker.pktco.in/whotopay");
-    this.sendGet("http://pool.pkteer.com");
-    this.sendGet("ttp://pool.pktpool.io");
-    this.sendGet("http://pool.pkt.world");
-    this.sendGet("http://pool.srizbi.com");
-    this.sendGet("http://noworries.tech/pool");
-    JSONObject poolJSON = new JSONObject();
-
-    return poolJSON;
+  public HashMap<String, Integer> getPoolStats() throws Exception {
+    HashMap<String, Integer> encryptionsPerSecondByPool = new HashMap<String, Integer>();
+    encryptionsPerSecondByPool.put("pktco", this.getPoolStats("http://paymaker.pktco.in/whotopay").getTotalEncryptionsPerSecond());
+    return encryptionsPerSecondByPool;
   };
-
-//  public void sendPost() throws Exception {
-//
-//    // form parameters
-//    Map<Object, Object> data = new HashMap<>();
-//    data.put("username", "abc");
-//    data.put("password", "123");
-//    data.put("custom", "secret");
-//    data.put("ts", System.currentTimeMillis());
-//
-//    HttpRequest request = HttpRequest.newBuilder()
-//      .POST(buildFormDataFromMap(data))
-//      .uri(URI.create("http://paymaker.pktco.in/whotopay"))
-//      .setHeader("User-Agent", "Java 11 HttpClient Bot") // add request header
-//      .header("Content-Type", "application/x-www-form-urlencoded")
-//      .build();
-//
-//    HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-//
-//    // print status code
-//    System.out.println(response.statusCode());
-//
-//    // print response body
-//    System.out.println(response.body());
-//
-//  }
 
   public static HttpRequest.BodyPublisher buildFormDataFromMap(Map<Object, Object> data) {
     var builder = new StringBuilder();
